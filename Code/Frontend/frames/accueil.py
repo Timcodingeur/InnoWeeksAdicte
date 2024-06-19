f45b918d63f0c0093bb138f5528dacc4dcb81ce5
jujumstein <131999436+jujumstein@users.noreply.github.com>
2024-06-19 15:28:59 +0200

    Update accueil.py

--- Code/Frontend/frames/accueil.py
+++ Code/Frontend/frames/accueil.py
@@ -4,17 +4,6 @@ import os
 
 images_path = 'Images'
 
-
-
-
-
-
-
-
-
-
-
-
 class Accueil(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg="#B7B6C1", bd=0, highlightthickness=0)
@@ -29,14 +18,8 @@ class Accueil(tk.Frame):
         self.task_label = tk.Label(self.card, text="", font=("Helvetica", 14), bg="#D5CFE1", fg="black", bd=0)
         self.task_label.pack(pady=10, padx=10)
 
-
-
-
-
         self.update_task()
     
-
-
     def update_task(self):
         for widget in self.card.winfo_children():
             widget.destroy()
@@ -51,28 +34,16 @@ class Accueil(tk.Frame):
                     task_label = tk.Label(task_frame, text=f"Tâche courante: {task['nom']} - {task['description']} - {task['nbpoints']}⌬", font=("Helvetica", 14), bg="#D5CFE1", fg="black", bd=0)
                     task_label.pack(side="left", pady=10, padx=10)
                     
-
-
-
-
-
-
-
-
                     task_label = tk.Entry(task_frame, font=("Helvetica", 12))
-
 
                     self.toggle_btn = tk.Button(task_frame, image=self.show_image, font=("Helvetica", 8), bg="#E83030", fg="white", command=self.toggle_image)
                     self.toggle_btn.pack(side=tk.LEFT, padx=(0, 0))
-
 
             else:
                 tk.Label(self.card, text="Aucune tâche courante", font=("Helvetica", 14), bg="#D5CFE1", fg="black", bd=0).pack(pady=10, padx=10)
         else:
             tk.Label(self.card, text="Aucune tâche courante", font=("Helvetica", 14), bg="#D5CFE1", fg="black", bd=0).pack(pady=10, padx=10)
     
-
-
     def toggle_image(self):
         if self.toggle_btn.cget('bg') == "#E83030":
             self.toggle_btn.config(image=self.hide_image)
