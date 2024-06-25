8c0420a2972374b2f455bc55adb8751c471861b6
benjaminnnnnnnnnnnnn <167529563+benjaminnnnnnnnnnnnn@users.noreply.github.com>
2024-06-25 14:59:40 +0200

    Add files via upload

--- Code/Frontend/frames/lootbox.py
+++ Code/Frontend/frames/lootbox.py
@@ -1,9 +1,14 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
 import requests
 import os
 import io
+import cv2 
+import numpy as np 
+
+# Create a VideoCapture object and read from input file 
+cap = cv2.VideoCapture(r'C:\Users\px25twk\Downloads\InnoWeeksAdicte-LeChat\InnoWeeksAdicte-LeChat\Code\Frontend\Images\lootbox_animation.mp4') 
 
 class Lootbox(tk.Frame):
     def __init__(self, parent, controller):
@@ -14,7 +19,7 @@ class Lootbox(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        label = tk.Label(self, text="Boîtes à butin aléatoire", font=("Helvetica", 14), bg="#D5CFE1", fg="black", bd=0)
+        label = tk.Label(self, text="Page de Lootbox", font=("Helvetica", 14), bg="#D5CFE1", fg="black", bd=0)
         label.pack(pady=10, padx=10)
 
         container_frame = tk.Frame(self, bg="#D5CFE1")
@@ -22,7 +27,6 @@ class Lootbox(tk.Frame):
 
         self.canvas = tk.Canvas(container_frame, bg="#D5CFE1", bd=0, highlightthickness=0)
         self.scrollable_frame = ttk.Frame(self.canvas, style="TFrame")
-
         self.scrollable_frame.bind(
             "<Configure>",
             lambda e: self.canvas.configure(
@@ -64,35 +68,32 @@ class Lootbox(tk.Frame):
 
     def create_lootbox_card(self, lootbox):
         card = tk.Frame(self.scrollable_frame, bg="#D5CFE1", bd=2, relief="solid", width=350, height=300)
-        card.pack_propagate(0) 
+        card.pack_propagate(0)
         card.pack(pady=10, padx=10, fill="both", expand=True)
         
-        label = tk.Label(card, text=lootbox['nom'], font=("Helvetica", 14), bg="#D5CFE1", fg="black", bd=0)
-        label.pack(pady=8, padx=8)
-        
+        label_nom = tk.Label(card, text=f"{lootbox['nom']} - {lootbox['prix']} ⌬", font=("Helvetica", 14), bg="#D5CFE1", fg="black")
+        label_nom.pack(pady=8, padx=8)
+
         cardloot = tk.Frame(card, bg="#B7B6C1", bd=2, relief="solid")
         cardloot.pack(pady=10, padx=10, fill="both", expand=True)
-        
-        label = tk.Label(cardloot, text=lootbox['nom'], font=("Helvetica", 12), bg="#B7B6C1", fg="black")
-        label.pack(pady=10, padx=10)
         
         image_path = os.path.join(self.images_path, lootbox.get('image', 'lootbox.png'))
         if not os.path.exists(image_path):
             image_path = os.path.join(self.images_path, 'lootbox.png')
         
         image = Image.open(image_path)
-        image = image.resize((150, 150), Image.LANCZOS)  
+        image = image.resize((150, 150), Image.LANCZOS)
         photo = ImageTk.PhotoImage(image)
         
         image_label = tk.Label(cardloot, image=photo, bg="#B7B6C1")
-        image_label.image = photo  
+        image_label.image = photo
         image_label.pack(pady=10)
-        
-        label = tk.Label(cardloot, text=f"{lootbox['prix']}⌬", font=("Helvetica", 14), bg="#B7B6C1", fg="black", bd=0)
-        label.pack(pady=8, padx=8)
-        
-        open_button = tk.Button(cardloot, text="Ouvrir", command=lambda: self.open_lootbox(lootbox['id']))
-        open_button.pack(pady=10)
+
+        # Binding the click event to the card
+        card.bind("<Button-1>", lambda event, id=lootbox['id']: self.open_lootbox(id))
+        cardloot.bind("<Button-1>", lambda event, id=lootbox['id']: self.open_lootbox(id))
+        label_nom.bind("<Button-1>", lambda event, id=lootbox['id']: self.open_lootbox(id))
+        image_label.bind("<Button-1>", lambda event, id=lootbox['id']: self.open_lootbox(id))
 
     def open_lootbox(self, lootbox_id):
         url = f"http://localhost:3000/api/lootbox/{lootbox_id}/ouvrir"
@@ -102,73 +103,64 @@ class Lootbox(tk.Frame):
             response.raise_for_status()
             recompense = response.json()['data']['recompense']
             self.display_reward(recompense)
+            fetch_and_update_user_info(self.controller)  # Mise à jour des informations de l'utilisateur
         except requests.exceptions.RequestException as e:
-            tk.messagebox.showerror("Erreur", f"Erreur lors de l'ouverture de la lootbox: {e}")
+            messagebox.showerror("Erreur", f"Erreur lors de l'ouverture de la lootbox: {e}")
 
     def display_reward(self, recompense):
         reward_window = tk.Toplevel(self)
         reward_window.title("Récompense")
         reward_window.geometry("400x400")
         reward_window.configure(bg="#D5CFE1")
-        
-        reward_window.update_idletasks()
-        x = (reward_window.winfo_screenwidth() - reward_window.winfo_reqwidth()) // 2
-        y = (reward_window.winfo_screenheight() - reward_window.winfo_reqheight()) // 2
-        reward_window.geometry(f"+{x}+{y}")
+
+
+        # Check if camera opened successfully 
+        if (cap.isOpened()== False): 
+            print("Error opening video file") 
+  
+        # Read until video is completed 
+        while(cap.isOpened()): 
+      
+            # Capture frame-by-frame 
+            ret, frame = cap.read() 
+            if ret == True: 
+                # Display the resulting frame 
+                cv2.imshow('Frame', frame) 
+          
+            # Press Q on keyboard to exit 
+                if cv2.waitKey(25) & 0xFF == ord('q'): 
+                    break
+  
+        # Break the loop 
+            else: 
+                break
+  
+        # When everything done, release 
+        # the video capture object 
+        cap.release() 
+  
+        # Closes all the frames 
+        cv2.destroyAllWindows() 
+
+
 
         reward_label = tk.Label(reward_window, text=recompense['nom'], font=("Helvetica", 12), bg="#D5CFE1", fg="black")
         reward_label.pack(pady=10, padx=10)
 
-   
         image_data = recompense['image']['data']
         image = Image.open(io.BytesIO(bytearray(image_data)))
-        image = image.resize((150, 150), Image.LANCZOS)  
+        image = image.resize((150, 150), Image.LANCZOS)
         photo = ImageTk.PhotoImage(image)
 
         image_label = tk.Label(reward_window, image=photo, bg="#D5CFE1")
-        image_label.image = photo 
+        image_label.image = photo
         image_label.pack(pady=10)
-
-        self.animate_text(reward_label, recompense['nom'])
 
         close_button = tk.Button(reward_window, text="Fermer", command=reward_window.destroy)
         close_button.pack(pady=10)
 
-    def animate_text(self, label, text):
-        def rotate_text():
-            current_text = label.cget("text")
-            new_text = current_text[-1] + current_text[:-1]
-            label.config(text=new_text)
-            label.after(200, rotate_text)
-        
-        label.config(text=text)
-        rotate_text()
-
     def display_default_message(self, message):
-        for widget in self.scrollable_frame.winfo_children():
-            widget.destroy()
+        label = tk.Label(self.scrollable_frame, text=message, font=("Helvetica", 14), bg="#D5CFE1", fg="black")
+        label.pack(pady=20, padx=20)
 
-        card = tk.Frame(self.scrollable_frame, bg="#D5CFE1", bd=2, relief="solid", width=350, height=200)
-        card.pack_propagate(0)
-        card.pack(pady=10, padx=10, fill="both", expand=True)
-        
-        label = tk.Label(card, text=message, font=("Helvetica", 14), bg="#D5CFE1", fg="black", bd=0)
-        label.pack(pady=8, padx=8)
-        
-        cardloot = tk.Frame(card, bg="#B7B6C1", bd=2, relief="solid")
-        cardloot.pack(pady=10, padx=10, fill="both", expand=True)
-        
-        label = tk.Label(cardloot, text="Lootbox par défaut", font=("Helvetica", 12), bg="#B7B6C1", fg="black")
-        label.pack(pady=10, padx=10)
-        
-        image_path = os.path.join(self.images_path, 'lootbox.png')  
-        image = Image.open(image_path)
-        image = image.resize((150, 150), Image.LANCZOS) 
-        photo = ImageTk.PhotoImage(image)
-        
-        image_label = tk.Label(cardloot, image=photo, bg="#B7B6C1")
-        image_label.image = photo  
-        image_label.pack(pady=10)
-
-        label = tk.Label(cardloot, text="N/A", font=("Helvetica", 14), bg="#B7B6C1", fg="black", bd=0)
-        label.pack(pady=8, padx=8)
+# Assurez-vous que `fetch_and_update_user_info` est correctement définie dans votre contrôleur principal
